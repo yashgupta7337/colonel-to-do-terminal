562bce68e6ffd7c0f5d686d757967c946a1f4e5c
Yash Gupta <ID+username@users.noreply.github.com>
2024-05-04 17:26:15 +0530

    import dependencies in index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import chalk from "chalk";
+import chalkAnimation from "chalk-animation";
+import gradient from "gradient-string";
+import inquirer from "inquirer";
+import figlet from "figlet";
+import { createSpinner } from "nanospinner";
