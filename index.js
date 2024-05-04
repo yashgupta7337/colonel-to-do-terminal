5d19151f14820cb5cff95daa1f741747b47c5917
Yash Gupta <ID+username@users.noreply.github.com>
2024-05-04 17:47:51 +0530

    neon greeting and colonel info

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 #!/usr/bin/env node
+
 import chalk from "chalk";
 import chalkAnimation from "chalk-animation";
 import gradient from "gradient-string";
 import inquirer from "inquirer";
 import figlet from "figlet";
 import { createSpinner } from "nanospinner";
+
+const USER = "Yash";
+
+async function welcomeUser() {
+  const rainbowGreetingText = chalkAnimation.glitch(`Hi, ${USER} \n`);
+  await new Promise((resolve) => {
+    setTimeout(resolve, 2000);
+  });
+  rainbowGreetingText.stop();
+
+  console.log(`
+  ${chalk.bgBlue("Colonel is a node CLI app")}
+  `);
+}
+
+await welcomeUser();
